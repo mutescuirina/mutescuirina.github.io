a410fe5ec36f9bdb7a79223b00b2b7f1dc5d7d93
acsirina <irina.a@irinas-MBP.home>
2019-04-07 18:53:04 -0400

    api is working and retruning an object

--- js/app.js
+++ js/app.js
@@ -1,31 +1,64 @@
 
+ //document ready  
 
 
 
-
-// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
-// xhr.done(function(data) { console.log("success got data", data); });
-
-
+    const apiData = () => {
+       
+        let userInput = $('#input').val();
+    userInput = userInput.trim().replace(/ /g, "+");
+    const queryUrl = 'http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=BgZkpbDDpfxjmUaEPwZuc7w8yLfDZZPy&=5';
+    // console.log(queryUrl)
     
+    const queryData = $.get(queryUrl);
+    queryData.done(function(response) { 
+        queryResponse = response.data[0].images.fixed_height.url;
+        console.log("success got data", response); 
+        $('.content_img').attr('src',queryResponse);});
     
-$(() => {
-$('button').on('click', ()=> {
+    }
+   
+
+
+
+    $(() => {
+
+$('#submit').on('click', ()=> {
     event.preventDefault()
-    const userInput = $('#input').val().trim();
-// userInput = userInput.replace(/ /g, "+");
-const queryUrl = 'http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=BgZkpbDDpfxjmUaEPwZuc7w8yLfDZZPy';
-    $.ajax({
-        url: queryUrl,
-        method: 'GET'
-    }).done(function(response) {
-        // console.log("Retrieved " + response.data + " records from the dataset!");
-    const testGiphy = response.data[0].images.fixed_height.url;
-    console.log(testGiphy);
- $('.content_img').attr('src',testGiphy);
-    
-})
-    
-})
+    apiData();
+
 
 })
+
+$('#reset').on('click', () => {
+    $(".content_img").attr('src', '')
+        
+    })
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////Graveyard//////////////////////////////////////////////////////////
+// //     $.ajax({
+// //         url: queryUrl,
+// //         method: 'GET',
+// //         limit: 5
+// //     }).done(function(response) {
+// //         console.log("Retrieved " + response.data + " records from the dataset!");
+// //     const testGiphy = response.data[0].images.fixed_height.url;
+// //     // console.log(testGiphy);
+// //  
+    // var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+// xhr.done(function(data) { console.log("success got data", data); });
+// })
+    
+// })
+// 
+});
