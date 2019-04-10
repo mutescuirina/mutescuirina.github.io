140c64c5362b6579ebf699207e7a461c8ff274ed
acsirina <irina.a@irinas-MBP.home>
2019-04-10 15:31:53 -0400

    styling about 50% done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//document ready  
+  
  // fisher yates shuffle
  // https://bost.ocks.org/mike/shuffle/
 
@@ -22,26 +22,49 @@
   
 //DATA GATHERING///////////////////////////////////////////////////////////////
     const apiData = () => {
-       
+        
         let userInput = $('#input').val();
     userInput = userInput.trim().replace(/ /g, "+");
-    const queryUrl = 'http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=BgZkpbDDpfxjmUaEPwZuc7w8yLfDZZPy';
+    const queryUrl = 'http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=BgZkpbDDpfxjmUaEPwZuc7w8yLfDZZPy&limit=10';
     // console.log(queryUrl)
     
     const queryData = $.get(queryUrl);
+
     queryData.done(function(response) { 
-
+        console.log(response.data[0])
         shuffle(response.data)
-        console.log("success got data", response); 
-        queryResponse = response.data[0].images.fixed_height.url;
-
+        console.log(response.data[0])
+    queryResponse = response.data[0].images.fixed_height.url
+    embedURL = response.data[0].embed_url
+    // console.log(embedURL);
+    
+    console.log("success got data", response); 
         const $img = $('<img>').attr('src',queryResponse)
+        const $embedCode = $('<input>').val(embedURL)
+        
         $('.content_img').append($img)
+        $('.embeded_code').append($embedCode)
     });
     
     }
+    
+    // i need a function that will will remove the previous img and displays just one 
+    const showOneImg = () => {
+        currentImg = $('.content_img').children().last()
+        if(('.content_img').length > 1)
+        currentImg.remove();
+        currentCode = $('.embeded_code').children().last()
+        if(('.embeded_code').length > 1)
+        currentCode.remove();
+        
+    }
    
+// const showMore = () => {
+//     const content = $('.content')
+//     for ( let i in content) {
 
+//     }
+// }
 
 
 //things that i need the button to do 
@@ -52,63 +75,70 @@
 
 //DOCUMENT READY///////////////////////////////////////////
     $(() => {
+ $('#search').on('click', ()=> {
+  // console.log('its working')
+    event.preventDefault();
+     apiData();
+     showOneImg();
+     $('#next').css('display', "block")
+               
+ })
+            
+
+$('#next').on('click', () => {
+    event.preventDefault();
+    apiData();
+    showOneImg();
+    
+})
 
 
-const favBtn = $('#favBtn')
-const favoriteArr = [];
+
+//Tuesday's obituaries/////////////////////////////////////
+
+
+
+// const favBtn = $('#favBtn')
+// const favoriteArr = [];
 
  
-const addFavorite = () => {
-    let favGify = $('.content_img').children()
-    favoriteArr.push(favGify)
-    for (i = 5; i > favoriteArr.length ; i--) {
-        alert('enough');
-    }
-    apiData()
+// const addFavorite = () => {
+//     let favGify = $('.content_img').children()
+//     favoriteArr.push(favGify)
+//     apiData()
     
     
    
     
 
-}
-const renderArr = () => {
-    for ( i = 0; i < favoriteArr.length; i++) {
-        $('.favorites').append(favoriteArr[i]);
-console.log(favoriteArr)
-    }
+// }
+// const renderArr = () => {
+//     for ( i = 0; i < favoriteArr.length; i++) {
+//         $('.favorites').append(favoriteArr[i]);
+// console.log(favoriteArr)
+//     }
     
-} 
+// } 
 
 
-///CLIKING EVENTS///////////////////////////////////////////////////////////////////////        
-$('#submit').on('click', ()=> {
-    // console.log('its working')
-    event.preventDefault();
-    apiData();
-    $('#input').val('');
-    
-})
+// ///CLIKING EVENTS///////////////////////////////////////////////////////////////////////        
+// 
 
 
 
-$(favBtn).on('click',() => {
-    event.preventDefault()
-    addFavorite();
-    renderArr();
+// $(favBtn).on('click',() => {
+//     event.preventDefault()
+//     addFavorite();
+//     renderArr();
 
-//if item is already in the favoriteArr then the button should not work 
-// it should not add the same item twice
-})
-$('#reset').on('click', () => {
-    console.log('hello')
+// //if item is already in the favoriteArr then the button should not work 
+// // it should not add the same item twice
+// })
+// $('#reset').on('click', () => {
+//     console.log('hello')
 
-    $(".content_img").attr('src', '')
-    })
-
-
-
-
-
+//     $(".content_img").attr('src', '')
+//     })
 
 
 
