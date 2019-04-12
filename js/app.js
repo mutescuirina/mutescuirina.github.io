40d01f433f7a6ddd61c2e518bb555545451b5463
acsirina <irina.a@irinas-MacBook-Pro.local>
2019-04-12 11:06:38 -0400

    fixed the @media

--- js/app.js
+++ js/app.js
@@ -71,7 +71,7 @@ const generateColor = () => {
     let hexValues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e"];
 
     function populate(a) {
-        for (var i = 0; i < 6; i++) {
+        for (let i = 0; i < 6; i++) {
             let x = Math.round(Math.random() * 14);
             let y = hexValues[x];
             a += y;
